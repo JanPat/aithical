5fa7e6012aa4028ff0bdfe113d18cc0e1afe786f
JanPat <janvi01@gmail.com>
2023-06-09 15:25:32 -0400

    input integration

--- api/api.py
+++ api/api.py
@@ -1,11 +1,17 @@
 import time
-from flask import Flask
+from flask import Flask, request, redirect, url_for, jsonify
+
 
 app = Flask(__name__)
 
+@app.route('/api/projectDescription', methods=['POST'])
+def send_input():
+    form_data = request.get_json()
+
+    return redirect(url_for('get_output_dict'))
+
 @app.route('/api/output_dict')
 def get_output_dict():
-
     output_dict = {
         'regulations': [
             {
@@ -28,7 +34,6 @@ def get_output_dict():
             }
         ],
         'guidelines': [
-
         ]
     }
     

34a517974411cadbd055d896b961990887821aea
JanPat <janvi01@gmail.com>
2023-06-09 15:33:03 -0400

    working input integration

--- api/api.py
+++ api/api.py
@@ -1,41 +1,78 @@
 import time
 from flask import Flask, request, redirect, url_for, jsonify
 
+from random import randrange
+
+import random
+
+
 
 app = Flask(__name__)
 
 @app.route('/api/projectDescription', methods=['POST'])
 def send_input():
     form_data = request.get_json()
-
     return redirect(url_for('get_output_dict'))
+
 
 @app.route('/api/output_dict')
 def get_output_dict():
-    output_dict = {
-        'regulations': [
-            {
-                'title' : "regulation_1",
-                'summary' : "regulation_1_description",
-                'link' : "regulation_1_link",
-                'source': "regulation_1_source"
-            },
-            {
-                'title' : "regulation_2",
-                'summary' : "regulation_2_description",
-                'link' : "regulation_2_link",
-                'source': "regulation_2_source"
-            },
-            {
-                'title' : "regulation_3",
-                'summary' : "regulation_3_description",
-                'link' : "regulation_3_link",
-                'source': "regulation_3_source"
-            }
-        ],
-        'guidelines': [
-        ]
-    }
+
+    x = random.randint(1, 6)
+
+    if x % 2 == 0:
+
+
+
+        output_dict = {
+            'regulations': [
+                {
+                    'title' : "regulation_1",
+                    'summary' : "regulation_1_description",
+                    'link' : "regulation_1_link",
+                    'source': "regulation_1_source"
+                },
+                {
+                    'title' : "regulation_2",
+                    'summary' : "regulation_2_description",
+                    'link' : "regulation_2_link",
+                    'source': "regulation_2_source"
+                },
+                {
+                    'title' : "regulation_3",
+                    'summary' : "regulation_3_description",
+                    'link' : "regulation_3_link",
+                    'source': "regulation_3_source"
+                }
+            ],
+            'guidelines': [
+            ]
+        }
+    else:
+            output_dict = {
+            'regulations': [
+                {
+                    'title' : "regulation_11",
+                    'summary' : "regulation_1_description",
+                    'link' : "regulation_1_link",
+                    'source': "regulation_1_source"
+                },
+                {
+                    'title' : "regulation_12",
+                    'summary' : "regulation_2_description",
+                    'link' : "regulation_2_link",
+                    'source': "regulation_2_source"
+                },
+                {
+                    'title' : "regulation_31",
+                    'summary' : "regulation_3_description",
+                    'link' : "regulation_3_link",
+                    'source': "regulation_3_source"
+                }
+            ],
+            'guidelines': [
+            ]
+        }
     
    
 

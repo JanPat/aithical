edb74400f449c2c10a07480d538cacd43b5f8a75
milk-girl <avasgilmour@gmail.com>
2023-06-09 15:42:44 -0400

    Changing function calls

--- api/api.py
+++ api/api.py
@@ -6,42 +6,11 @@ app = Flask(__name__)
 @app.route('/api/output_dict')
 def get_output_dict():
 
-    output_dict = {
-        'regulations': [
-            {
-                'title' : "regulation_1",
-                'summary' : "regulation_1_description",
-                'link' : "regulation_1_link",
-                'source': "regulation_1_source"
-            },
-            {
-                'title' : "regulation_2",
-                'summary' : "regulation_2_description",
-                'link' : "regulation_2_link",
-                'source': "regulation_2_source"
-            },
-            {
-                'title' : "regulation_3",
-                'summary' : "regulation_3_description",
-                'link' : "regulation_3_link",
-                'source': "regulation_3_source"
-            }
-        ],
-        'guidelines': [
-
-        ]
-    }
-    
+ 
    
 
-    return output_dict
+    return classify_display()
 
-
-@app.route('/project-view')
-def get_classification():
-    bob = classify_display()
-    bob.update({'chicken' : 'wing'})
-    return bob
 
 
 # main() -- put all other functions in the backend folder - also rename main()
